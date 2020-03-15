0f808b2dedb45581e2f1744e9fcc51412fa1fd08
srinisa <sai.vignesh.srinivasan@navis.com>
2020-03-15 11:19:06 +0530

    intent

--- app/intentResolvers/index.js
+++ app/intentResolvers/index.js
@@ -3,6 +3,7 @@ import {
   productsBasedOnCategory,
   constructTextResponse,
   getUserDetails,
+  addOrUpdateUser,
 } from '../utils/helpers';
 import { SHOW_PRODUCT_CATALOG_INTENT, VIEW_PRODUCT, WELCOME_MESSAGE } from '../utils/constants';
 
@@ -13,8 +14,9 @@ const resolveIntent = async ({ intentName = '', parameters = {} , request}) => {
     case WELCOME_MESSAGE: {
       console.log('inside welcome message');
       const userContext = getUserDetails(request)
-      console.log('userContext -=> ' + userContext)
-      responseObject = constructTextResponse('User');
+      console.log('userContext -=> ' + userContext);
+      addOrUpdateUser(userContext);
+      responseObject = constructTextResponse('Hello ' + userContext.first_name);
       break;
     }
     case SHOW_PRODUCT_CATALOG_INTENT: {
